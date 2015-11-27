38f71bb54a0e5cbcdc28652e93ff714d8e5e0368
gilbertogomez <jose.gilberto.gomez@gmail.com>
2015-11-26 21:09:02 -0600

    nuevo diseño

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,15 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
 window.$ = window.jQuery = require("jquery");
 
-$(window).load(function () {});
+$(window).load(function () {
+	$('#main').addClass('animated zoomIn');
+
+	var options = [{ selector: '.fadeIn1', offset: 100, callback: 'Materialize.fadeInImage(".fadeIn1")' }, { selector: '#educacion', offset: 200, callback: '$("#rollIn").addClass("animated rollIn")' }, { selector: '#experiencia', offset: 200, callback: '$("#slideInRight").addClass("animated slideInRight")' }];
+	Materialize.scrollFire(options);
+	$(".button-collapse").sideNav();
+});
 
 },{"jquery":2}],2:[function(require,module,exports){
 /*!
